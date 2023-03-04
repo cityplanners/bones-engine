77c402910b48e77424a83dc8f3e29d0c0bd5a58b
cpadilla <christoferpadilla@gmail.com>
2023-03-03 23:13:39 -0800

    Adding entity/component system.

---
+++ src/ecs/entity.rs
@@ -0,0 +1,5 @@
+
+#[derive(Clone, Copy, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Entity {
+    index: usize
+}
---
+++ src/ecs/world.rs
@@ -0,0 +1,70 @@
+use std::cell::{RefCell, RefMut};
+use crate::ecs::component::*;
+
+pub struct World {
+    entity_count: usize,
+    component_vecs: Vec<Box<dyn ComponentVec>>
+}
+
+impl World {
+    pub fn new() -> World {
+        Self {
+            entity_count: 0,
+            component_vecs: Vec::new()
+        }
+    }
+
+    fn new_entity(&mut self) -> usize {
+        let entity_id = self.entity_count;
+        for component_vec in self.component_vecs.iter_mut() {
+            component_vec.push_none();
+        }
+        self.entity_count += 1;
+        entity_id
+    }
+
+    fn add_component_to_entity<ComponentType: 'static>(
+        &mut self,
+        entity: usize,
+        component: ComponentType,
+    ) {
+        // Search for any existing ComponentVecs that match the type of the component being added.
+        for component_vec in self.component_vecs.iter_mut() {
+            if let Some(component_vec) = component_vec
+                .as_any_mut()
+                .downcast_mut::<RefCell<Vec<Option<ComponentType>>>>()
+            {
+                component_vec.borrow_mut()[entity] = Some(component);
+                return;
+            }
+        }
+
+        // No matching component storage exists yet, so we have to make one.
+        let mut new_component_vec: Vec<Option<ComponentType>> =
+            Vec::with_capacity(self.entity_count);
+
+        // All existing entities don't have this component, so we give them `None`
+        for _ in 0..self.entity_count {
+            new_component_vec.push(None);
+        }
+
+        // Give this Entity the Component.
+        new_component_vec[entity] = Some(component);
+        self.component_vecs
+            .push(Box::new(RefCell::new(new_component_vec)));
+    }
+
+    fn borrow_component_vec_mut<ComponentType: 'static>(
+        &self,
+    ) -> Option<RefMut<Vec<Option<ComponentType>>>> {
+        for component_vec in self.component_vecs.iter() {
+            if let Some(component_vec) = component_vec
+                .as_any()
+                .downcast_ref::<RefCell<Vec<Option<ComponentType>>>>()
+            {
+                return Some(component_vec.borrow_mut());
+            }
+        }
+        None
+    }
+}
